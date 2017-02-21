56b0eb1a3aa73b26e589dca3d4b7926b5fb34757
Lachlan Enderlin <lendo1995@gmail.com>
2017-02-21 10:57:29 +1000

    Saving the basic framework

---
+++ app/views/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <script src="<?= URL; ?>public/js/default.js"></script>
+        <script src="<?= URL; ?>public/js/<?= $this->fileName; ?>.js"></script>
+        <link href="<?= URL; ?>public/css/default.css" rel="stylesheet" />
+        <link href="<?= URL; ?>public/css/<?= $this->fileName;?>.css" rel="stylesheet" />
+        <link href="<?= URL; ?>public/css/media.css" rel="stylesheet" />
+        <link rel="stylesheet" href="<?= URL; ?>public/css/fa/css/font-awesome.min.css">
+    </head>
+    <body>
+        <header></header>
+        <main>
